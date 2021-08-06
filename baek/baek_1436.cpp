dff08b8e49610c896db08230bcce59370bdd0792
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-07 02:20:21 +0900

    .

---
+++ baek/baek_1436.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, cnt = 0;
+    int title = 665; // 초기값
+    cin >> N;
+    string S;
+
+    while (++title)
+    {
+        s = to_string(title);
+
+        if (s.find("666") != -1)
+        { // 찾았다!
+            ++cnt;
+        }
+
+        if (cnt == N)
+        {
+            cout << title << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
