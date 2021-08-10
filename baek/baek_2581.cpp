c7c48dfc78957bb8e27b89731e91e15ebd518ab1
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-11 07:07:10 +0900

    BOJ 2581 Solve

--- baek/baek_2581.cpp
+++ baek/baek_2581.cpp
@@ -1,36 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool arr[10001];
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int min, max;
-    cin >> min >> max;
     int sum = 0;
-    bool prime[max + 1];
-    fill_n(prime, max + 1, 1);
 
-    prime[0] = false;
-    prime[1] = false;
+    int min_num = 10001;
+    cin >> min >> max;
+    fill_n(arr, 10001, true);
+    arr[0] = false;
+    arr[1] = false;
 
-    for (int i = 2; i <= sqrt(max); i++)
+    for (int i = 2; i < sqrt(max); i++)
     {
-        if (prime[i] == true)
+        if (arr[i] == true)
         {
-            for (int j = i * 2; j <= max; j++)
+            for (int j = i * 2; j <= max; j += i)
             {
-                prime[j] = false;
+                arr[j] = false;
             }
         }
     }
 
     for (int i = min; i <= max; i++)
     {
-        if (prime[i] == true)
+        if (arr[i] == true)
         {
-            cout << i << "\n";
+            if (min_num > i)
+                min_num = i;
+            sum += i;
         }
+    }
+
+    if (sum == 0)
+    {
+        cout << "-1\n";
+    }
+    else
+    {
+        cout << sum << "\n"
+             << min_num << "\n";
     }
 }
