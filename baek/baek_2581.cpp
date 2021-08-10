cce34b7c164556efabcfe52e925aba9aab8bb19b
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-11 06:18:01 +0900

    .

--- baek/baek_2581.cpp
+++ baek/baek_2581.cpp
@@ -1,25 +1,36 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int min, max;
-    bool *prime;
-    cin >> min;
-    cin >> max;
-    prime = new bool[max + 1];
+    cin >> min >> max;
+    int sum = 0;
+    bool prime[max + 1];
     fill_n(prime, max + 1, 1);
+
     prime[0] = false;
     prime[1] = false;
 
     for (int i = 2; i <= sqrt(max); i++)
+    {
         if (prime[i] == true)
-            for (int j = i * 2; j <= max; j += i)
+        {
+            for (int j = i * 2; j <= max; j++)
+            {
                 prime[j] = false;
+            }
+        }
+    }
 
     for (int i = min; i <= max; i++)
+    {
         if (prime[i] == true)
+        {
             cout << i << "\n";
-    return 0;
+        }
+    }
 }
