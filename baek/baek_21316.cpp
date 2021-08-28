e4e63698e880ab4a71b4e8bbfbec8634c239b1c2
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-28 17:16:12 +0900

    Solved BOJ 7568

---
+++ baek/baek_21316.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    int K;
+    int small_jump[N - 1];
+    int big_jump[N - 1];
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        scanf("%d %d", &small_jump[i], &big_jump[i]);
+    }
+
+    scanf("%d", &K);
+
+    
+
+
+}
--- baek/baek_7568.cpp
+++ baek/baek_7568.cpp
@@ -1,9 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int weight[50];
+int height[50];
+int rank_dungchi[50];
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    i
+    // 사람의 수
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> weight[i] >> height[i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (i != j)
+            {
+                if (weight[j] > weight[i] && height[j] > height[i])
+                {
+                    rank_dungchi[i]++;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << rank_dungchi[i] + 1 << " ";
+    }
+
+    cout << "\n";
 }
