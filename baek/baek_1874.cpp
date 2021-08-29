d7789b2aeb14e09eb74683c5b932b83468fb3fc8
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-29 15:35:28 +0900

    Solved BOJ 1874

--- baek/baek_1874.cpp
+++ baek/baek_1874.cpp
@@ -2,16 +2,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MX = 100001;
+int num[MX];
+vector<char> ret;
+stack<int> st;
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int N;
-
     cin >> N;
 
     for (int i = 0; i < N; i++)
     {
+        cin >> num[i];
     }
+
+    int pos = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        // 기본적으로 하는 행동
+        st.push(i);
+        ret.push_back('+');
+
+        while (!st.empty() && st.top() == num[pos])
+        {
+            pos++;
+            ret.push_back('-');
+            st.pop();
+        }
+    }
+
+    if (!st.empty())
+        cout << "NO\n";
+    else
+    {
+        for (int i = 0; i < ret.size(); i++)
+            cout << ret[i] << "\n";
+    }
+
+    return 0;
 }
