1c7a3e7fc44a67ac1b81d30eeef146ba9f78a27b
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-29 15:57:35 +0900

    Add Remark To BOJ 1874

--- baek/baek_1874.cpp
+++ baek/baek_1874.cpp
@@ -1,4 +1,3 @@
-// Unsolve
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -24,9 +23,11 @@ int main()
     for (int i = 1; i <= N; i++)
     {
         // 기본적으로 하는 행동
+        // 바로 top에 있는 수와 일치하다고 해도 + - 로 pop 댐
         st.push(i);
         ret.push_back('+');
 
+        // pop 부분
         while (!st.empty() && st.top() == num[pos])
         {
             pos++;
@@ -35,13 +36,20 @@ int main()
         }
     }
 
+    /*
+    결과 출력 부분
+    */
     if (!st.empty())
+        // 스택에 숫자가 남아있을 때 => 불가능한 수열이 주어짐
+        // NO 출력
         cout << "NO\n";
     else
     {
+        //
         for (int i = 0; i < ret.size(); i++)
             cout << ret[i] << "\n";
     }
 
+    // 정상적으로 프로그램 종료
     return 0;
 }
