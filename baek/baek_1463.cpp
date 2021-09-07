9ba8d960c875092174d177f2fcc204854c4cd930
caputdraconis050630 <caputdraconis@kakao.com>
2021-09-07 17:30:23 +0900

    Solved BOJ 9095

--- baek/baek_1463.cpp
+++ baek/baek_1463.cpp
@@ -2,61 +2,29 @@
 using namespace std;
 
 int arr[1000005];
-int dx[3] = {1, 2, 3};
+int n;
 
-int main()
+int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
-
-    int n;
+    // fill(arr, arr + 1000005, INT_MAX);
     cin >> n;
+
     arr[1] = 0;
-    queue<int> Q;
-    Q.push(1);
-    while (!Q.empty())
+    for (int i = 2; i <= n; i++)
     {
-        int cur = Q.front();
-        Q.pop();
-
-        //종료 조건
-        if (cur == n)
+        arr[i] = arr[i - 1] + 1;
+        if (i % 2 == 0)
         {
-            cout << arr[cur];
-            return 0;
+            arr[i] = min(arr[i], arr[i / 2] + 1);
         }
-
-        int nx;
-
-        for (int dir = 0; dir < 3; dir++)
+        if (i % 3 == 0)
         {
-
-            //1칸 더하기
-            if (dir == 0)
-            {
-                nx = cur + dx[0];
-            } // x2 or x3
-            else
-            {
-                nx = cur * dx[dir];
-            }
-
-            //범위 넘어가면 스킵
-            if (nx > n)
-                continue;
-
-            //방문한적 없으면 연산횟수+1 저장
-            if (arr[nx] == 0)
-            {
-                arr[nx] = arr[cur] + 1;
-            } //방문한적 있으면 스킵
-            else
-            {
-                continue;
-            }
-
-            Q.push(nx);
+            arr[i] = min(arr[i], arr[i / 3] + 1);
         }
     }
+
+    cout << arr[n] << "\n";
+    return 0;
 }
--- baek/baek_9095.cpp
+++ baek/baek_9095.cpp
@@ -1,11 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int arr[11];
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int T;
-    cin>>T;
-    
+    cin >> T;
+
+    arr[1] = 1;
+    arr[2] = 2;
+    arr[3] = 4;
+    // 1 1 1 / 1 2 / 2 1 / 3
+
+    for (int i = 4; i < 11; i++)
+    {
+        arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+    }
+
+    while (T--)
+    {
+        int tmp;
+        cin >> tmp;
+        cout << arr[tmp] << "\n";
+    }
+    return 0;
 }
