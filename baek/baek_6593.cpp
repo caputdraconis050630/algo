016e8b7d957c24369291e0c32164e6246b53208a
caputdraconis050630 <caputdraconis@kakao.com>
2021-09-17 06:05:23 +0900

    Unsolve BOJ 6593

---
+++ baek/baek_6593.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int L, R, C;
+/*
+    L : 층 수
+    R : 한 층의 행
+    C : 한 층의 열
+*/
+
+char board[35][35][35];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> L >> R >> C;
+
+    while (!(L == 0 && R == 0 && C == 0))
+    {
+        for (int i = 0; i < L; i++)
+        {
+        }
+
+        cin >> L >> R >> C; //다음 테스트 케이스를 위한 입력
+    }
+}
