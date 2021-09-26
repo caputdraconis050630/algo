c3fda75399c35c97659e6e3feda7d347406b6f06
caputdraconis050630 <caputdraconis@kakao.com>
2021-09-26 22:43:30 +0900

    Solve BOJ 11724

--- baek/baek_11724.cpp
+++ baek/baek_11724.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
+int N, M;
+vector<int> V[1001];
+bool chk[1001]; // 방문 체크v
+int ans;
+
+void dfs(int x)
+{
+    chk[x] = true;
+
+    for (int i = 0; i < V[x].size(); i++)
+    {
+        int next = V[x][i];
+        if (!chk[next])
+        {
+            dfs(next);
+        }
+    }
+}
 
 int main(void)
 {
@@ -9,5 +26,25 @@ int main(void)
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >>
+    cin >> N >> M; // N : 정점의 개수  M : 연결 요소의 개수
+
+    int x, y;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> x >> y;
+
+        V[x].push_back(y);
+        V[y].push_back(x);
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (chk[i] == false)
+        {
+            ans++;
+            dfs(i);
+        }
+    }
+
+    cout << ans << '\n';
 }
