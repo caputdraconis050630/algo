8a80664e78c87c9c25f6c4386750bbe52d313f38
caputdraconis050630 <caputdraconis@kakao.com>
2021-09-29 07:21:43 +0900

    Unsolve BOJ 7569

---
+++ baek/baek_7569.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct tomato
+{
+    int x, y, z;
+} tomato;
+
+int M, N, H;
+int board[102][102][102];
+int days[102][102][102];
+queue<tomato> Q;
+bool flag = false;
+
+const int dx[] = {-1, 1, 0, 0, 0, 0};
+const int dy[] = {0, 0, -1, 1, 0, 0};
+const int dz[] = {0, 0, 0, 0, -1, 1};
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    cin >> M >> N >> H;
+
+    fill(&days[0][0][0], &days[0][0][0] + (int)pow(102, 3), -1);
+
+    for (int i = 0; i < H; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < M; k++)
+            {
+                cin >> board[i][j][k];
+                /*
+                    if board[i][j][k] == -1
+                        not exist
+                    if board[i][j][k] == 0
+                        not riped
+                    if board[i][j][k] == 1
+                        riped
+                    
+                */
+
+                if (board[i][j][k] == 1)
+                {
+                    tomato tmp;
+                    tmp.x = i;
+                    tmp.y = j;
+                    tmp.z = k;
+                    days[tmp.x][tmp.y][tmp.z] = 0;
+                    Q.push(tmp);
+                }
+                else if (board[i][j][k] == 0)
+                {
+                    flag = true;
+                }
+            }
+        }
+    }
+
+    if (!flag)
+    {
+        cout << "0\n";
+        return 0;
+    }
+
+    while (!Q.empty())
+    {
+        tomato cur = Q.front();
+        Q.pop();
+
+        for (int dir = 0; dir < 6; dir++)
+        {
+            tomato nxt;
+            nxt.x = cur.x + dx[dir];
+            nxt.y = cur.y + dy[dir];
+            nxt.z = cur.z + dz[dir];
+
+            // 범위 필터링
+            if (nxt.x >= 0 && nxt.x < H && nxt.y >= 0 && nxt.y < N && nxt.z >= 0 && nxt.z < M)
+            {
+                continue;
+            }
+
+            if (days[nxt.x][nxt.y][nxt.y] != -1)
+            {
+                // 이미 방문한 적이 있다면 continue
+                continue;
+            }
+
+            if (board[nxt.x][nxt.y][nxt.y] != 0)
+            {
+                // 그 칸이 이미 익은 토마토이거나, 토마토가 존재하지 않을 때 continue
+                continue;
+            }
+
+            days[nxt.x][nxt.y][nxt.y] = days[cur.x][cur.y][cur.z] + 1;
+            board[nxt.x][nxt.y][nxt.y] = 1;
+            Q.push(nxt);
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < H; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < M; k++)
+            {
+                if (board[i][j][k] == 0)
+                {
+                    cout << "-1\n";
+                    return 0;
+                }
+
+                if (ans < days[i][j][k])
+                    ans = days[i][j][k];
+            }
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
