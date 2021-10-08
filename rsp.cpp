6b5dd8625c96f709b77a9dff5c2f7d7c13397585
caputdraconis050630 <caputdraconis@kakao.com>
2021-10-08 20:50:25 +0900

    Wonderful Beautiful Awesome Prog

---
+++ rsp.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Solve()
+{
+    srand(time(NULL));
+    int ran = rand() % 3;
+    cout << "너를 승리로 이끌 손모양 >> ";
+
+    if (ran == 0)
+    {
+        cout << "가위\n";
+    }
+    else if (ran == 1)
+    {
+        cout << "바위\n";
+    }
+    else
+    {
+        cout << "보\n";
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    Solve();
+    return 0;
+}
