72aa3436746db0011ddf93113bf059a6cc41168f
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-02 16:20:56 +0900

    Unsolve BOJ 16236

---
+++ baek/baek_16236.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define first X
+#define second Y
+using namespace std;
+
+int N;
+int size = 2; // 아기 상어의 기본 사이즈 2
+int board[21][21];
+int visit[21][21];
+
+int dx[4] = {1, 0, 0, -1};
+int dy[4] = {0, -1, 1, 0};
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    queue<pair<int, int>> Q;
+    fill(&visit[0][0], &visit[20][21], -1); // Not visit -> -1
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> board[i][j];
+
+            if (board[i][j] == 9)
+            {
+                Q.push(make_pair(i, j));
+                visit[i][j] = 0;
+            }
+        }
+    }
+
+    pair<int, int> cur;
+    while (!Q.empty)
+    {
+        cur = Q.front();
+        Q.pop();
+
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+
+            if (board[nx][ny] == 0)
+            {
+                // 빈칸일 때
+                continue;
+            }
+
+            if (board[nx][ny] > size)
+            {
+                // 아기 상어의 사이즈보다 클 경우 continue
+                continue;
+            }
+        }
+    }
+
+    int MX = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (visit[i][j] > MX)
+                MX = visit[i][j];
+        }
+    }
+
+    cout << MX << '\n';
+    return 0;
+}
--- baek/baek_5525.cpp
+++ baek/baek_5525.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int N, M;
 string S;
+int result = 0;
 
 int main(void)
 {
@@ -13,12 +14,10 @@ int main(void)
 
     cin >> S;
 
-    int ans = 0;
     for (int i = 0; i < M; i++)
     {
-        int k = 0; // k는 IOI의 길이. IOI면 1 , IOIOI면 2
-        // O가 나오면 pass
-        if (S[i] == 'O')
+        int k = 0;
+        if (S[i] == 'O') // 시작이 I이기 때문에
             continue;
         // s[i] == 'I'일 때
         else
@@ -29,14 +28,14 @@ int main(void)
                 if (k == N)
                 {
                     k--; // 중복 카운트를 막기 위해 -1 해준다.
-                    ans++;
+                    result++;
                 }
                 i += 2; // O를 건너 뛴다.
             }
             k = 0;
         }
     }
-    cout << ans;
+    cout << result;
 
     return 0;
 }
