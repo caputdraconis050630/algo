e0dc54300e1089f16c3ed1c984e54f38f25e382e
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-14 04:23:23 +0900

    Solve BOJ 16204

---
+++ baek/baek_16204.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N, M, K;
+
+    int ao = M;
+    int ax = N - M;
+
+    int bo = K;
+    int bx = N - K;
+
+    int ans = 0;
+    ans += min(ao, bo);
+    ans += min(ax, bx);
+    cout << ans << "\n";
+}
