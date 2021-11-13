317efbce6e54ee71afe0c9a8668a833d57202c5b
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-14 01:18:45 +0900

    Solve BOJ 15439

---
+++ baek/baek_15439.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    cout << N * (N - 1) << "\n";
+}
