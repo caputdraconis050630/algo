75ce1ef2396fa771d791aee1bdf2bd9407f361c1
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-14 01:04:20 +0900

    Solve BOJ 8370

---
+++ baek/baek_8370.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int n1, n2, k1, k2;
+
+    cin >> n1 >> k1 >> n2 >> k2;
+    cout << n1 * k1 + n2 * k2 << "\n";
+}
