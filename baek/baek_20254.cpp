0c2e29ce91172b85dbd339d601019c4cc8e31e0e
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-14 01:43:17 +0900

    Solve BOJ 20254

---
+++ baek/baek_20254.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << a * 56 + b * 24 + c * 14 + d * 6 << "\n";
+}
