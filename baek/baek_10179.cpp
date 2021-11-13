d254ebe264aebf7e551c9f00d708a53c0e244241
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-14 04:55:06 +0900

    Solve BOJ 10179

---
+++ baek/baek_10179.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        double tmp;
+        cin >> tmp;
+
+    
+        printf("$%0.2lf\n", tmp*0.8);
+    }
+}
