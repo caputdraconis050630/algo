ca787f3ec70186eb785245b048da2e5c8daafd62
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-14 02:32:08 +0900

    Solve BOJ 2420

---
+++ baek/baek_2420.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    ll N, M;
+    cin >> N >> M;
+
+    cout << abs(M - N) << "\n";
+}
