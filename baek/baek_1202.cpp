76f3fa4c8631f420a996df17353a90727dac02a2
caputdraconis050630 <caputdraconis@kakao.com>
2021-11-17 07:51:55 +0900

    Solve BOJ 1202

---
+++ baek/baek_1202.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K;
+    cin >> N >> K;
+
+    pair<int, int> jew[300005]; // 가격 무게
+    multiset<int> bag;
+
+    for (int i = 0; i < N; i++)
+    {
+        pair<int, int> tmp;
+        cin >> tmp.second >> tmp.first;
+
+        jew[i] = tmp;
+    }
+
+    for (int i = 0; i < K; i++)
+    {
+        int tmp;
+        cin >> tmp;
+
+        bag.insert(tmp);
+    }
+
+    sort(jew, jew + N); // 가격을 기준으로 오름차순으로 정렬
+
+    long long ans = 0;
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        pair<int, int> cur = jew[i];
+
+        auto it = bag.lower_bound(cur.second);
+
+        if (it == bag.end())
+            continue;
+        ans += cur.first;
+
+        bag.erase(it);
+    }
+
+    cout << ans << "\n";
+}
