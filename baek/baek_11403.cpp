5613ea923a725962061c1212f49b2628e3736483
caputdraconis050630 <caputdraconis@kakao.com>
2021-12-27 16:54:40 +0900

    Unsolve BOJ 11403

--- baek/baek_11403.cpp
+++ baek/baek_11403.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int N;
+int board[101][101];
+vector<int> arr[101];
+int main(void)
+{
     ios::sync_with_stdio(0);
     cin.tie(NULL);
 
-    
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> board[i][j];
+            arr[i].push_back(j);
+        }
+    }
 }
+
+/*
+0 - > 1
+1 - > 2
+2 - > 0
+
+
+
+*/
