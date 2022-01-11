379d2c2cad7a691335b2fd598a09ab9f077c79ff
caputdraconis <caputdraconis@kakao.com>
2022-01-11 13:48:59 +0900

    220111 algorithm

---
+++ programmers/카펫.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow)
+{
+    vector<int> answer;
+    int sum = brown + yellow;
+
+    for (int col = 3; col <= 987654321; col++)
+    {
+        for (int row = 3; row <= col; row++)
+        {
+            if (col < row)
+                break;
+
+            if ((col - 2) * (row - 2) == yellow && row * col == sum)
+            {
+
+                answer.push_back(col);
+                answer.push_back(row);
+                col = 987654322;
+                break;
+            }
+            else if (col * row > brown + yellow)
+            {
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
