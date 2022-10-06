b98ef0259151dd240e10b5ed2bf856faea776009
Guntak Kim <caputdraconis@kakao.com>
2022-10-06 15:19:04 +0900

    20221006 LEET Better Code

--- leet/valid-palindrome.py
+++ leet/valid-palindrome.py
@@ -1,34 +1,13 @@
 class Solution:
-	def isNonAlphaNumeric(self, c) -> bool:
-			# NonAlphaNumeric -> False
-			# AlphaNumeric -> True
-			if c.isalnum():
-				return False
-			return True
-		
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
 
-	def isPalindrome(self, s: str) -> bool:
-		s = s.lower()
-		len_s = len(s) - 1
-		i = 0
-		j = len_s
-		while i < j:
-			while self.isNonAlphaNumeric(s[i]):
-				if i < len_s:
-					i += 1
-				else:
-					break
-			while self.isNonAlphaNumeric(s[j]):
-				if j > 0:
-					j -= 1
-				else:
-					break
-			if (i > j):
-				return True
-			if s[i] != s[j]:
-				return False
-			else:
-				if i < j:
-					i += 1
-					j -= 1
-		return True
+        # 팰린드롬 여부 판별
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
