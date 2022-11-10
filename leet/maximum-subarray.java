eb118527b188f44d9d99be9f668fac5c0cf58047
Guntak Kim <caputdraconis@kakao.com>
2022-11-10 16:26:14 +0900

    Leet Data Structure Plan

---
+++ leet/maximum-subarray.java
@@ -0,0 +1,14 @@
+package leet;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+		int currSum = nums[0];
+		int n = nums.length;
+		for (int i=1; i<n; i++) {
+			currSum = Math.max(currSum + nums[i], nums[i]);
+			maxSum = Math.max(maxSum, currSum);
+		}
+		return maxSum;
+    }
+}
